29d46cce87a7cd702dfc474ea827bb92874ddf2f
yogesh18140 <43813000+yogesh18140@users.noreply.github.com>
2018-10-26 14:25:32 +0530

    Lemmatizing updated
    updated code for lemmatizing
    relative path replaces absolute path
    output is in csv format now

--- PreProcessing.py
+++ PreProcessing.py
@@ -3,10 +3,13 @@ import math
 import pandas
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from nltk import ngrams
+from nltk import pos_tag
+from collections import Counter
 
 class PreProcessing:
     def __init__(self):
-        self.data_set_path = "/home/yogi/Desktop/Webapps Data"                   #Path of data set folder
+        self.data_set_path = "Webapps Data"                                      #Path of data set folder
         self.data_files = os.listdir(self.data_set_path)                         #List of all the file names with extension in dataset folder
         self.file_word_dictionary = {}                                           #Dictionary of files containing various words.
         self.file_sentence_dictionary ={}                                        #Dictionary of files containing all the sentences
@@ -16,6 +19,8 @@ class PreProcessing:
         self.word_set={}                                                         #It is the union of all the different words in corpus
         self.stop_words =set(stopwords.words("english"))                         #It contains all the stop words in english language
         self.file_names =[]                                                      #Contain File names without extension
+        self.file_bigram_dictionary ={}                                          #Contain all bigrams (word appear 2 times)
+        self.file_pos_tag ={}                                                    #Contains tags for each file   
         
     def setFileName(self):
         for name in self.data_files:
@@ -25,26 +30,57 @@ class PreProcessing:
          word=word.strip('\n,:()"!@#$%^&*=+-.\'1234567890;:<>/')                         #from various punctuation marks.
          return word
     
+    def getPosTags(self,tag):
+        if tag.startswith('J'):		# adjective
+            return 'a'
+        elif tag.startswith('V'):	# verb
+            return 'v'
+        elif tag.startswith('N'):	# noun
+            return 'n'
+        elif tag.startswith('R'):	# adverb
+            return 'r'
+        else:
+            return ''
+
+    def getBigrams(self,word_set):
+        bigrams=list(ngrams(word_set,2))
+        return bigrams
+    
     def splitFileToLines(self,sentence_set,file_content):               #Splitting the files into lines
         for line in file_content:                                       #Sentence_set is the dictionary which is initially empty
             sentences = line.split("\n.")                               #file_content is the content of the particular file
             for sentence in sentences:
                 sentence=sentence.strip('\n ')
                 if(sentence!=''):
-                    sentence_set.append(sentence)
+                    sentence_set.append(sentence.lower())
         return sentence_set
     
     def splitFileToWords(self,word_set,file_content):                   #Splitting the files into words
+        newTag=[]
+        WN=WordNetLemmatizer()
         for line in file_content:                                       #word_set is the dictionary which is initially empty
             words = line.split(" ")                                     #file_content is the content of the particular file
-            WN=WordNetLemmatizer()
             for word in words:
+                word=word.lower()
                 #word=self.dataCleaning(word)                           #function not required as isalpha used below
-                if word.lower() not in self.stop_words:                 #checking if word not in stop word
-                    word=WN.lemmatize(word.lower())
+                if word not in self.stop_words:                         #checking if word not in stop word
+                    #word=WN.lemmatize(word)
                     if(word!=''and len(word)>2 and word.isalpha()):     #isalpha to filter punctuation
                         word_set.append(word)
-        return word_set
+        newTag=pos_tag(word_set)
+        lemmatizedWords=[]
+        for i in range(len(word_set)):                      #Lemmetizing the word file
+            #if(word_set[i]=='writing'):
+                #print(WN.lemmatize(word_set[i],'n'))
+                #print(newTag[i][1])
+            if(self.getPosTags(newTag[i][1]) == ''):
+                lemma= WN.lemmatize(word_set[i])
+            elif self.getPosTags(newTag[i][1]) == 'r' and word_set[i].endswith('ly'):
+                lemma = word_set[i].replace('ly','')
+            else:
+                lemma = WN.lemmatize(word_set[i], pos=self.getPosTags(newTag[i][1]))
+            lemmatizedWords.append(lemma)
+        return lemmatizedWords,newTag
     
     def fileRead(self):                                                 #Method to read all the dataset files 
         for file in self.data_files:
@@ -53,11 +89,18 @@ class PreProcessing:
             file__content =  open(actual_path,"r")
             word_set = []
             sentence_set =[]
-            word_set = self.splitFileToWords(word_set,file__content)
+            bigram_set =[]
+            tags=[]
+            word_set,newTag= self.splitFileToWords(word_set,file__content)
+            tags+=newTag
             sentence_set = self.splitFileToLines(sentence_set,file_content)
+            bigram_set = self.getBigrams(word_set)
+            #tags=pos_tag(word_set)
             self.file_sentence_dictionary[file[:-4]] = sentence_set
             self.file_word_dictionary[file[:-4]] = word_set
-        return self.file_word_dictionary, self.file_sentence_dictionary
+            self.file_bigram_dictionary[file[:-4]] = bigram_set
+            self.file_pos_tag[file[:-4]]=tags
+        return self.file_word_dictionary, self.file_sentence_dictionary,self.file_bigram_dictionary,self.file_pos_tag
     
     def getWordSet(self):                          #IT gives the collection of all the different words in corpus
         for file_list in self.file_word_dictionary:
@@ -98,7 +141,7 @@ class PreProcessing:
         return tfidf
 
     def initiate(self):                             #Main function of the class
-        self.file_word_dictionary, self.file_sentence_dictionary=self.fileRead()
+        self.file_word_dictionary, self.file_sentence_dictionary,self.file_bigram_dictionary,self.file_pos_tag=self.fileRead()
         self.word_set = self.getWordSet()
         self.file_word_frequency = self.computeWordFrequency()
         self.setFileName()
@@ -116,9 +159,9 @@ class PreProcessing:
         df=pandas.DataFrame({'Data':self.file_sentence_dictionary[file]} for file in self.file_sentence_dictionary)     #2nd Dataframe
         df.index=[x for x in self.file_names]
         df_combined = pandas.concat([df,df2],axis =1)
-        writer = pandas.ExcelWriter('DataFiles.xlsx')
-        df_combined.to_excel(writer,'Sheet1')
-        writer.save()
+
+        df_combined.to_csv("DataFiles.csv",sep=',')                         #Converting to csv format
+        
         
 def main():
     PreprocessObj = PreProcessing()
